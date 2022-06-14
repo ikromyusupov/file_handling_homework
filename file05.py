920aabc3eee2974393a9f299c923f641ab340811
Ikrom <ikromyusupov09@gmail.com>
2022-06-14 15:58:42 +0500

    Find the number of digital and str(non-digital)

--- file05.py
+++ file05.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    digit = 0
+    non_digit = 0
+    for line in data:
+        for char in line:
+            if char.isdigit():
+                digit += 1
+            else:
+                non_digit += 1
+    return [digit, non_digit]
 # Read data from file
+fayl = open('txt_file/data05.py','r')
+data = fayl.read()
